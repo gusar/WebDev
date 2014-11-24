fcb9909954cc3bccb47bd8ce2cfdf24eaaa8c3ee
Andy Lahs <andy.lahs@gmail.com>
2014-11-24 14:56:12 +0000

    reworked validation for registration

--- WebDevA/registerSQL.php
+++ WebDevA/registerSQL.php
@@ -1,40 +1,73 @@
 <?php
 if(isset($_POST['register'])) {
-	// Check if all required fields have been filled 
-	if($_POST["rusername"] && $_POST["rpassword1"] 
-		&& $_POST["rpassword2"] && $_POST["rfirstname"]
-		&& $_POST["rsurname"] && $_POST["raddressline1"]
-		&& $_POST["rcity"] && $_POST["rtelephone"])
-	{
-		// Check if the two passwords match
-		if($_POST["rpassword1"] == $_POST["rpassword2"])
-		{
-			include("connectDB.php");
+	$value = validate();
 
-			$query = "SELECT username FROM Users WHERE username LIKE '$_POST[rusername]'";
-			$result = mysql_query($query, $conn) or die(mysql_error());
-			if(mysql_num_rows($result) < 1) {
-
-				// Insert into database and check if successful
-				$query = "INSERT INTO Users VALUES (
-								'$_POST[rusername]', 
-								'$_POST[rpassword1]', 
-								'$_POST[rfirstname]', 
-								'$_POST[rsurname]', 
-								'$_POST[raddressline1]', 
-								'$_POST[raddressline2]', 
-								'$_POST[rcity]', 
-								'$_POST[rtelephone]', 
-								'$_POST[rmobile]')";
-				$result = mysql_query($query, $conn) or die(mysql_error());
-
-				mysql_close($conn);
-				header("location: login.php?r=1");
-			}
-			else print "Error: That username already exists";
-		}
-		else print "Error: Passwords don't match";
+	switch($value) {
+		case 0:
+			header("location: login.php?r=1");
+			break;
+		case 1:
+			print"Error: Enter all information marked with *";
+			break;
+		case 2:
+			print "Error: Phone numbers must be numeric";
+			break;
+		case 3:
+			print "Error: Passwords don't match or too long";
+			break;
+		case 5:
+			print "Error: That username already exists";
 	}
-	else print"Error: Enter all information marked with *";
+}
+
+
+function validate() {
+	// Check if all required fields have been filled 
+	if(!$_POST["rusername"] || !$_POST["rpassword1"] 
+		|| !$_POST["rpassword2"] || !$_POST["rfirstname"]
+		|| !$_POST["rsurname"] || !$_POST["raddressline1"]
+		|| !$_POST["rcity"] || !$_POST["rtelephone"])
+	{
+		return(1);
+	}
+
+	// Check if phone numbers are numeric
+	if(!preg_match("#[0-9]+#",$_POST["rtelephone"])) {
+		return(2);
+	}
+
+	if($_POST["rmobile"] && !preg_match("#[0-9]+#",$_POST["rmobile"])) {
+		return(2);
+	}
+	
+	// Check if the two passwords match and are under 6 chars long
+	if($_POST["rpassword1"] != $_POST["rpassword2"]) {
+		return(3);
+	}
+	
+	include("connectDB.php");
+
+	$query = "SELECT username FROM Users WHERE username LIKE '$_POST[rusername]'";
+	$result = mysql_query($query, $conn) or die(mysql_error());
+
+	if(mysql_num_rows($result) > 1) {
+		return(4);
+	}
+
+	// Insert into database and check if successful
+	$query = "INSERT INTO Users VALUES (
+					'$_POST[rusername]', 
+					'$_POST[rpassword1]', 
+					'$_POST[rfirstname]', 
+					'$_POST[rsurname]', 
+					'$_POST[raddressline1]', 
+					'$_POST[raddressline2]', 
+					'$_POST[rcity]', 
+					'$_POST[rtelephone]', 
+					'$_POST[rmobile]')";
+	$result = mysql_query($query, $conn) or die(mysql_error());
+
+	mysql_close($conn);
+	return(0);
 }
 ?>
